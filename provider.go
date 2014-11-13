8bb9478cb7554e6a72da8f3eeab8d10a46751f86
Svend Sorensen <ssorensen@whitepages.com>
2014-11-13 15:33:40 -0800

    Switch to terraform.ResourceProvider

--- provider.go
+++ provider.go
@@ -2,10 +2,11 @@ package main
 
 import (
 	"github.com/svend/terraform-provider-dummy/Godeps/_workspace/src/github.com/hashicorp/terraform/helper/schema"
+	"github.com/svend/terraform-provider-dummy/Godeps/_workspace/src/github.com/hashicorp/terraform/terraform"
 )
 
 // Provider returns a terraform.ResourceProvider.
-func Provider() *schema.Provider {
+func Provider() terraform.ResourceProvider {
 	return &schema.Provider{
 		ResourcesMap: map[string]*schema.Resource{
 			"dummy_server": resourceServer(),
